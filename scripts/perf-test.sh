2464bd3521b7268bff0f5f6863f3c8f47656c8b0
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2023-02-11 11:17:44 -0500

    Adding perf test script.

---
+++ scripts/perf-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NUMBER_OF_RUNS=20
+
+echo "Running console perf tests $NUMBER_OF_RUNS times"
+for i in $( seq 0 $NUMBER_OF_RUNS); 
+do
+  /usr/bin/time -a -o console-times-output.txt node index.js -f console -p >> console-output.txt
+done
+
+echo "Running pino perf tests $NUMBER_OF_RUNS times"
+for i in $( seq 0 $NUMBER_OF_RUNS); 
+do
+  /usr/bin/time -a -o pino-times-output.txt node index.js -f pino -p >> pino-output.txt
+done
+
+echo "Running winston perf tests $NUMBER_OF_RUNS times"
+for i in $( seq 0 $NUMBER_OF_RUNS); 
+do
+  /usr/bin/time -a -o winston-times-output.txt node index.js -f winston -p >> winston-output.txt
+done
+
+echo "Running log4js perf tests $NUMBER_OF_RUNS times"
+for i in $( seq 0 $NUMBER_OF_RUNS); 
+do
+  /usr/bin/time -a -o log4js-times-output.txt node index.js -f log4js -p >> log4js-output.txt
+done
+
+echo "Done!"
+
+
